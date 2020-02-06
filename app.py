5cc2ad44ecf140242626f92ccf8bb6c059a1b97c
JM100 <jbenemourad@gmail.com>
2020-02-06 08:56:54 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+set(stopwords.words('english'))
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    stop_words = stopwords.words('english')
+    text1 = request.form['text1'].lower()
+
+    processed_doc1 = ' '.join([word for word in text1.split() if word not in stop_words])
+
+    sa = SentimentIntensityAnalyzer()
+    dd = sa.polarity_scores(text=text1)
+    compound = round((1 + dd['compound'])/2, 2)
+
+    return render_template('form.html', final=compound, text1=text1)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5002, threaded=True)
